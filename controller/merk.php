21c256d84ef77b890a82299346588ef11ef52f7f
pawit <wahib05jhf@gmail.com>
2019-07-17 02:50:48 +0700

    view data

--- controller/merk.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nama_merk = $_POST['nama_merk'];
-
-if (isset($_POST['submit'])) {
-    include_once("../database/koneksi.php");
-    mysqli_query($koneksi, "INSERT INTO produk(nama_merk) VALUES('$nama_merk')");
-
-    // header("location:daftar_barang.php");
-}
--- controller/view_data.php
+++
@@ -1,3 +0,0 @@
-<?php
-include_once("../database/koneksi.php");
-$hasil = mysqli_fetch_array(mysqli_query($koneksi, "SELECT * FROM `printer`"), MYSQLI_ASSOC);
